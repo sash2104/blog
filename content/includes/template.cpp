75d58571deba8a773a68553345c9109784acdd2c
sash <sash2104@gmail.com>
2019-06-16 12:08:25 +0900

    :bug: 修正漏れ

--- content/includes/template.cpp
+++ content/includes/template.cpp
@@ -6,4 +6,4 @@ typedef unsigned long long ull;
 typedef pair<int,int> pii;
 typedef pair<ll, ll> pll;
 template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
-template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return true; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
